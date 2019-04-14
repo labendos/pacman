7da7a6cbea196c510a7a0aef70d05d63bdfbe6bb
labendos <47711984+labendos@users.noreply.github.com>
2019-04-14 17:25:08 +0300

    Update Character.h

--- Character.h
+++ Character.h
@@ -5,15 +5,35 @@ class Character
 {
 protected:
     int r;
-    double x;
-    double y;
-    int xi;
-    int yi;
     int count;
     int mov;
     double vel;
 public:
+    double x;
+    double y;
+    int xi;
+    int yi;
     virtual void draw(sf::RenderWindow* window) = 0;
+    int get_xi();
+    int get_yi();
+    int get_x();
+    int get_y();
 };
 
+int Character::get_xi()
+{
+    return xi;
+}
+int Character::get_yi()
+{
+    return yi;
+}
+int Character::get_x()
+{
+    return x;
+}
+int Character::get_y()
+{
+    return y;
+}
 #endif /* Character_h */
