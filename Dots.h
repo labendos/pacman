10958a7bd5dd7974265527e8677e07fc2ac8f5c1
labendos <47711984+labendos@users.noreply.github.com>
2019-04-14 17:28:43 +0300

    Update Dots.h

--- Dots.h
+++ Dots.h
@@ -1,17 +1,29 @@
 #ifndef Dots_h
 #define Dots_h
-
+#include "Map_Generating.h"
 class Dots
 {
+protected:
+    sf::SoundBuffer buffer;
+    sf::Sound sound;
     int map[N][N];
 public:
     Dots();
     void draw(sf::RenderWindow* window);
     void check(int i, int j);
+    void restart();
+    Dots& operator= (const Dots &dots)
+    {
+        for(int i = 0; i < N; i++)
+            for(int j = 0; j < N; j++)
+                map[i][j] = dots.map[i][j];
+        return *this;
+    }
 };
 
 Dots::Dots()
 {
+    
     for(int i = 0; i < N; i++)
     {
         for(int j = 0; j < N; j++)
@@ -19,6 +31,8 @@ Dots::Dots()
             map[i][j] = MAP[i][j];
         }
     }
+    buffer.loadFromFile(resourcePath() + "sound.wav");
+    sound.setBuffer(buffer);
 }
 
 void Dots::draw(sf::RenderWindow* window)
@@ -45,11 +59,28 @@ void Dots::check(int i, int j)
     if(map[i][j] == 0)
     {
         ++score;
-        if(score == 188)
+        sound.play();
+        /*sf::SoundBuffer buffer;
+        buffer.loadFromFile("sound.wav");
+        sf::Sound sound;
+        sound.setBuffer(buffer);*/
+        sound.play();
+        if(score == 100)
         {
             fail = -1;
         }
         map[i][j] = 2;
+    }
+}
+
+void Dots::restart()
+{
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            map[i][j] = MAP[i][j];
+        }
     }
 }
 
